2d3dfaa32f8d802b1f7a250cb723d030d6daebf8
Rahul <rktechnicalgroup69@gmail.com>
2024-03-02 20:49:18 +0530

    done his 301

--- Main.java
+++ Main.java
@@ -2,5 +2,8 @@ class Main{
     public static void main(String arg[]){
         System.out.println("Hello...");
         System.out.println("Hi...");
+
+        // HIS 301 
+        System.out.println("HIS 301");
     }
 }
